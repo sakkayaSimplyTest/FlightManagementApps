486947f3ee237b3c78491b2c657e8a640252f005
sakkaya <sakkaya@simlytest.de>
2021-12-24 20:11:46 +0100

    added new methods -public boolean equals(Object o) -public int hashCode() {

--- src/main/java/com/pluralsight/tddjunit5/airport/Passenger.java
+++ src/main/java/com/pluralsight/tddjunit5/airport/Passenger.java
@@ -1,5 +1,7 @@
 package com.pluralsight.tddjunit5.airport;
 
+import java.util.Objects;
+
 public class Passenger {
 
 	private String name;
@@ -17,5 +19,19 @@ public class Passenger {
 	public boolean isVip() {
 		return vip;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass())return false;
+		Passenger passenger = (Passenger) o;
+		return Objects.equals(name, passenger.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
 
 }
