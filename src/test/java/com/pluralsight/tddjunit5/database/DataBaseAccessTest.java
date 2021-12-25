2dfec3e7901969ef84b228a5516aefa3848f8731
sakkaya <sakkaya@simlytest.de>
2021-12-25 22:27:11 +0100

    DatBaseAccessTest with Database and Credentials by extending MockitoExtension

---
+++ src/test/java/com/pluralsight/tddjunit5/database/DataBaseAccessTest.java
@@ -0,0 +1,40 @@
+package com.pluralsight.tddjunit5.database;
+
+import com.pluralsight.tddjunit5.MockitoExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class DataBaseAccessTest {
+
+    @Mock
+    private Database database;
+
+    private Credentials credentials = new Credentials("user", "password");
+
+    @Test
+    void testUserSuccessfulLogin() {
+        when(database.login(credentials)).thenReturn(true);
+        Credentials sameCredentials = new Credentials("user", "password");
+        assertTrue(database.login(sameCredentials));
+
+    }
+
+    @Test
+    void testUserFailedLogin() {
+        when(database.login(credentials)).thenReturn(true);
+        Credentials otherCredentials = new Credentials("user", "password");
+        otherCredentials.setUsername("otherUser");
+        otherCredentials.setPassword("otherPassword");
+        assertNotEquals(credentials.getUsername(), otherCredentials.getUsername());
+        assertNotEquals(credentials.getPassword(), otherCredentials.getPassword());
+        assertNotEquals(credentials.hashCode(), otherCredentials.hashCode());
+        assertFalse(database.login(otherCredentials));
+    }
+
+
+}
