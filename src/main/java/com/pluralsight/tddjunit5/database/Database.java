ace4855e3122b40ee43177292a39d9f9259cc426
sakkaya <sakkaya@simlytest.de>
2021-12-25 23:08:24 +0100

    DatBaseAccessTest refactored StatisticsTest added DatabaseUtil added WITHOUT IMPLEMENTATIONS METHODS

--- src/main/java/com/pluralsight/tddjunit5/database/Database.java
+++ src/main/java/com/pluralsight/tddjunit5/database/Database.java
@@ -1,11 +1,14 @@
 package com.pluralsight.tddjunit5.database;
 
+import com.pluralsight.tddjunit5.airport.Flight;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Database {
+
 
     private Map<String, String> registeredUsers = new HashMap<>();
     List<List<String>> queriedData = new ArrayList<>();
@@ -21,4 +24,19 @@ public class Database {
     public List<List<String>> queryFlightsDatabase() {
         return queriedData;
     }
+
+    public double averageDistance(List<Flight> flightsList) {
+        return 0.0;
+    }
+
+    public int minimumDistance(List<Flight> flightsList) {
+        return 0;
+    }
+
+    public int maximumDistance(List<Flight> flightsList) {
+        return 0;
+    }
+
+
+
 }
